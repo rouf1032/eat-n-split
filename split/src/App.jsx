f23f8e3158e4000dea9089c91ab44d493364d37e
rouf1032 <rouf141263gmail.com>
2024-05-02 11:36:53 +0600

    fourth commit

--- split/src/App.jsx
+++ split/src/App.jsx
@@ -7,23 +7,32 @@ import Button from './Button'
 const FriendsList = [
   {id:101,name:"John Smidth",image:"https://i.postimg.cc/bYMvRkQy/pexels-olly-839011.jpg",balance:-7},
   {id:102,name:"Michel Clerck",image:"https://i.postimg.cc/TYgWLCZM/confident-attractive-caucasian-guy-beige-pullon-smiling-broadly-while-standing-against-gray.jpg",balance:125},
-  {id:103,name:"Sam Billing",image:"https://i.postimg.cc/gJ1nrZs2/inside-portrait-confident-young-man-white-clothes-posing-with-charming-smile-isolated-wall.jpg",balance:-125},
-  {id:104,name:"J. Walton",image:"https://i.postimg.cc/fW0Vp8Wt/handsome-bearded-guy-posing-against-white-wall.jpg",balance:0},
+  // {id:103,name:"Sam Billing",image:"https://i.postimg.cc/gJ1nrZs2/inside-portrait-confident-young-man-white-clothes-posing-with-charming-smile-isolated-wall.jpg",balance:-125},
+  // {id:104,name:"J. Walton",image:"https://i.postimg.cc/fW0Vp8Wt/handsome-bearded-guy-posing-against-white-wall.jpg",balance:0},
 ]
 
 function App() {
 
   const [showAddFriend, setShowAddFriend] = useState(false)
+  const [friends, setFriends] = useState(FriendsList)
+
+
+
+
   const handleShowAddFriend = () => {
     setShowAddFriend((show) => !show)
-    console.log(showAddFriend);
+    // console.log(showAddFriend);
+  }
+
+  const handleAddFriend = (friend) => {
+    setFriends((friends) =>[...friends,friend])
   }
 
   return (
     <div className='app'>
       <div className="sidebar">
-      <Friends friends={FriendsList}/>
-      {showAddFriend && <FormAddFriend />}
+      <Friends friends={friends}/>
+      {showAddFriend && <FormAddFriend handleAddFriend={handleAddFriend}/>}
       <Button onclick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add Friend'}</Button>
       </div>
       <FormSplitBill />
--- split/src/FormAddFriend/FormAddFriend.jsx
+++ split/src/FormAddFriend/FormAddFriend.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import './FormAddFriend.css'
-import Button from '../Button';
+import React, { useState } from "react";
+import "./FormAddFriend.css";
+import Button from "../Button";
 
-const FormAddFriend = () => {
-    return (
-        <form className="form-add-friend" >
+const FormAddFriend = ({handleAddFriend}) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleImage = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFriend = {
+      name,
+      image,
+      balance:0,
+      id:crypto.randomUUID()
+
+
+    }
+
+    handleAddFriend(newFriend);
+    setName("")
+    setImage("")
+  }
+
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend name</label>
-      <input
-        type="text"
-      />
+      <input type="text" value={name} onChange={handleName} />
 
       <label>🌄 Image URL</label>
-      <input
-        type="text"
-      />
+      <input type="text" value={image} onChange={handleImage} />
 
       <Button>Add</Button>
     </form>
-    );
+  );
 };
 
 export default FormAddFriend;
